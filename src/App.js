aaa6e6f38c841bdaa63065994c29354e24dd532e
Tiago Lopes Francisco <96237107+TiagoLFrancisco@users.noreply.github.com>
2023-06-28 23:07:46 +0100

    Add routing to App.js For Home, Ability Score, Charisma and Alignments

--- src/App.js
+++ src/App.js
@@ -1,10 +1,61 @@
+import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  useNavigate,
+} from "react-router-dom";
+import { BottomNavigation, BottomNavigationAction } from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
 import Home from "./components/Home";
+import AbilityScores from "./components/abilityscores/AbilityScores";
+import Alignments from "./components/alignments/Alignments";
+import AbilityCharisma from "./components/abilityscores/charisma/AbilityCharisma";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/ability-scores" element={<AbilityScores />} />
+          <Route path="/ability-scores/cha" element={<AbilityCharisma />} />
+          <Route path="/alignments" element={<Alignments />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+function Navigation() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/");
+  };
+
+  return (
+    <nav>
+      <BottomNavigation
+        showLabels
+        sx={{
+          justifyContent: "flex-start",
+          "& .Mui-selected": {
+            color: "dimgrey",
+          },
+        }}
+      >
+        <BottomNavigationAction
+          label="Home"
+          icon={<HomeIcon style={{ color: "#424242" }} />}
+          component={Link}
+          to="/"
+          onClick={handleClick}
+        />
+      </BottomNavigation>
+    </nav>
   );
 }
 
