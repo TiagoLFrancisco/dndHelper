8968df915e200a889ab33e6f35dd81800206c41d
Tiago Lopes Francisco <96237107+TiagoLFrancisco@users.noreply.github.com>
2023-06-28 23:19:41 +0100

    Separate items by listItems in charisma ability

--- src/components/abilityscores/charisma/AbilityCharisma.js
+++ src/components/abilityscores/charisma/AbilityCharisma.js
@@ -10,10 +10,11 @@ const abilityCharisma = {
   index: "cha",
   name: "CHA",
   full_name: "Charisma",
-  desc: [
-    "Charisma measures your ability to interact effectively with others. It includes such factors as confidence and eloquence, and it can represent a charming or commanding personality.",
-    "A Charisma check might arise when you try to influence or entertain others, when you try to make an impression or tell a convincing lie, or when you are navigating a tricky social situation. The Deception, Intimidation, Performance, and Persuasion skills reflect aptitude in certain kinds of Charisma checks.",
-  ],
+  desc: "Charisma measures your ability to interact effectively with others. It includes such factors as confidence and eloquence, and it can represent a charming or commanding personality.",
+  saving:
+    "A Charisma check might arise when you try to influence or entertain others, when you try to make an impression or tell a convincing lie, or when you are navigating a tricky social situation.",
+  skill_desc:
+    "The Deception, Intimidation, Performance, and Persuasion skills reflect aptitude in certain kinds of Charisma checks.",
   skills: [
     {
       name: "Deception",
@@ -45,23 +46,26 @@ function AbilityCharisma() {
       <List>
         <ListItem>
           <ListItemText
-            primary={abilityCharisma.name}
-            secondary={abilityCharisma.full_name}
+            primary={abilityCharisma.full_name}
+            secondary={abilityCharisma.name}
           />
         </ListItem>
         <ListItem>
-          <ListItemText primary="Description: " />
+          <ListItemText
+            primary={"Description"}
+            secondary={abilityCharisma.desc}
+          />
         </ListItem>
-        {abilityCharisma.desc.map((description, index) => (
-          <ListItem key={index}>
-            <ListItemText primary={description} />
-          </ListItem>
-        ))}
+        <ListItem>
+          <ListItemText primary={"Check"} secondary={abilityCharisma.saving} />
+        </ListItem>
       </List>
-
       <List className="horizontal-list">
         <ListItem>
-          <ListItemText primary="Affected Skills:" />
+          <ListItemText
+            primary="Affected Skills"
+            secondary={abilityCharisma.skill_desc}
+          />
         </ListItem>
         {abilityCharisma.skills.map((skill, index) => (
           <ListItem key={index} className="list-item">
