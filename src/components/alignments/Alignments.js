a4a4a6bbc143b7977db2444f809972675cb5c92a
Tiago Lopes Francisco <96237107+TiagoLFrancisco@users.noreply.github.com>
2023-06-29 18:51:39 +0100

    Add navigate from aligments to children

--- src/components/alignments/Alignments.js
+++ src/components/alignments/Alignments.js
@@ -4,10 +4,12 @@ import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import ListItemButton from "@mui/material/ListItemButton";
+import { useNavigate } from "react-router-dom";
 
 import "./Alignments.css";
 
 function Alignments() {
+  const navigate = useNavigate();
   const [alignmentsData, setAlignmentsData] = useState(null);
 
   useEffect(() => {
@@ -25,16 +27,18 @@ function Alignments() {
     }
   };
 
-  const handleListItemClick = () => {};
+  const handleListItemClick = (index) => {
+    navigate(index);
+  };
 
   return (
     <div className="list-container">
       {alignmentsData ? (
         <List className="horizontal-list">
-          {alignmentsData.results.map(({ index, name }) => (
+          {alignmentsData.results.map(({ index, name, url }) => (
             <ListItem key={index} className="list-item">
-              <ListItemButton onClick={() => handleListItemClick()}>
-                <ListItemText primary={name} secondary={index} />
+              <ListItemButton onClick={() => handleListItemClick(index)}>
+                <ListItemText primary={name} secondary={url} />
               </ListItemButton>
             </ListItem>
           ))}
