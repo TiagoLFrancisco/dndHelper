1af7b9a1be7621610fa965201d37dc819ff468cc
Tiago Lopes Francisco <96237107+TiagoLFrancisco@users.noreply.github.com>
2023-06-29 18:50:55 +0100

    Add ability's description

--- src/components/abilityscores/AbilityScores.js
+++ src/components/abilityscores/AbilityScores.js
@@ -42,14 +42,36 @@ function AbilityScores() {
     navigate(index);
   };
 
+  const abilityDescription = (
+    <p>
+      Is a character muscle-bound and insightful? Brilliant and charming? Nimble
+      and hardy?
+      <br />
+      <br />
+      Six abilities provide a quick description of every creature's physical and
+      mental characteristics:
+      <br />
+      Charisma, Dexterity, Constitution, Intelligence, Strength and Wisdom.
+      <br />
+      <br />
+      Ability scores define these qualities, a creature's assets as well as
+      weaknesses. The three main rolls of the game are the ability check, the
+      saving throw, and the attack roll and they rely on the six ability scores.
+      <br />
+      The basic rule behind these rolls: roll a d20, add an ability modifier
+      derived from one of the six ability scores, and compare the total to a
+      target number.
+    </p>
+  );
+
   return (
     <div className="list-container">
       {abilityScoresData ? (
         <List className="horizontal-list">
-          {abilityScoresData.map(({ name, full_name, index }) => (
+          {abilityScoresData.map(({ name, full_name, index, url }) => (
             <ListItem key={name} className="list-item">
               <ListItemButton onClick={() => handleListItemClick(index)}>
-                <ListItemText primary={full_name} secondary={name} />
+                <ListItemText primary={full_name} secondary={url} />
               </ListItemButton>
             </ListItem>
           ))}
@@ -57,6 +79,15 @@ function AbilityScores() {
       ) : (
         <p>Loading API data...</p>
       )}
+
+      <List>
+        <ListItem>
+          <ListItemText
+            primary={"Abilities"}
+            secondary={abilityDescription}
+          ></ListItemText>
+        </ListItem>
+      </List>
     </div>
   );
 }
